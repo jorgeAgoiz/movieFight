4003916bb44ff501eb466ef1245edcad7c65d5dc
jorgeAgoiz <jorgeagoiz@gmail.com>
2020-08-22 22:34:56 +0200

    show results in index.html
    Firsts steps to show the results in index.html last update for today 22/08

--- index.js
+++ index.js
@@ -7,15 +7,33 @@ const fetchData = async (searchMovie) => {
             s: searchMovie
         }
     });
+    if (response.data.Error){
+        return [];//If Error its true return an empty array to fix the error can not iterate.
+    }
+
     console.log(response.data);//Just for check in the console the results
+    return response.data.Search;
 };
 
 const movie1 = document.querySelector('#movie1');//Select input element in HTML
 
-const onInput = event => {//Call to api with fetchData function
-        fetchData(event.target.value);
+const onInput = async event => {//Call to api with fetchData function
+       const movies = await fetchData(event.target.value);
+       console.log(movies);
+
+       for (let movie of movies){
+           const divM = document.createElement('div');//Create an element div
+           divM.innerHTML = `
+                <img src="${movie.Poster}"/>
+                <h1>${movie.Title}<h1/>
+           `;//Insert HTML inside the div element
+           document.body.appendChild(divM);//And add to the body
+       }
+       
+
+       
 };
 
-movie1.addEventListener('input', debounce(onInput, 1000));//Event listener with onInput function
+movie1.addEventListener('input', debounce(onInput, 1000));//Event listener with onInput function and delay
 
 
