45a8f258bf981af68576a43dd0f2de58646e00f2
jorgeAP <jorgeagoiz@gmail.com>
2020-08-25 21:18:20 +0200

    update before refactoring
    I have the basic logic, but now Im going to refactor my code, to make more reusable. 25/08

--- index.js
+++ index.js
@@ -32,7 +32,12 @@ const resultsWrapper = document.querySelector('.results');//Select the results d
 
 const onInput = async event => {//Call to api with fetchData function
     const movies = await fetchData(event.target.value);
-    console.log(movies);
+
+    // If const movies is empty(delete the title from the input) remove the dropdown.
+    if(!movies.length){
+        dropdown.classList.remove('is-active');
+        return;
+    }
 
     resultsWrapper.innerHTML = ``;
     dropdown.classList.add('is-active');//When the call was finished add this class to the menu
@@ -45,6 +50,15 @@ const onInput = async event => {//Call to api with fetchData function
             <img src="${imgSrc}"/>
             ${movie.Title}
         `;//Insert HTML inside the <a> element
+
+        option.addEventListener('click', () => {//if you click in a movie, close again the dropdown and 
+            dropdown.classList.remove('is-active');// put the value in the input form of this movie
+            input.value = movie.Title;
+
+            //Now we need to do another request for the movie data
+            onSelectMovie(movie);
+        });
+
         resultsWrapper.appendChild(option);//And add to the a
     }
        
@@ -53,5 +67,62 @@ const onInput = async event => {//Call to api with fetchData function
 };
 
 input.addEventListener('input', debounce(onInput, 1500));//Event listener with onInput function and delay
+
+document.addEventListener('click', event => {// If you click outside the input, this event will does not 
+    if(!root.contains(event.target)){ // show the list of movies.
+        dropdown.classList.remove('is-active');
+    }
+    console.log(event.target);
+});
+
+// This function make a new request to show the data of movie selected
+const onSelectMovie = async movie => {
+
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params: {//Axios library allows to pass objects as arguments
+            apikey: '6092f0f8',
+            i: movie.imdbID
+        }
+    });
+
+    console.log(response.data);
+
+    document.querySelector('.summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+    `;
+};
 
 
